9eda44dff0130eea908a8b4c4f82e925d31d4d87
Marcelo Cárdenas <marcelo.cardenas.13@sansano.usm.cl>
2026-01-12 19:15:16 -0300

    Tested first ledger generation iteration

--- smallbanc/argparse.cpp
+++ smallbanc/argparse.cpp
@@ -26,6 +26,40 @@ void Parser::parse()
     {
       args_.get().ledger_location = std::string( argv_[++i] );
     }
+    else if ( argument == "--account-from" && i + 1 < argc_ )
+    {
+      args_.get().account_from = std::stoul( argv_[++i] );
+    }
+    else if ( argument == "--account-to" && i + 1 < argc_ )
+    {
+      args_.get().account_to = std::stoul( argv_[++i] );
+    }
+    else if ( argument == "--amount" && i + 1 < argc_ )
+    {
+      args_.get().amount = std::stod( argv_[++i] );
+    }
+    else if ( argument == "--description" && i + 1 < argc_ )
+    {
+      args_.get().description = std::string( argv_[++i] );
+    }
+    else if ( argument == "--type" && i + 1 < argc_ )
+    {
+      args_.get().type = std::string( argv_[++i] );
+    }
+    else if ( argument == "--account" && i + 1 < argc_ )
+    {
+      args_.get().account_number = std::stoul( argv_[++i] );
+    }
+    else if ( argument[0] != '-' && args_.get().command.empty() )
+    {
+      // First non flag is the command
+      args_.get().command = argument;
+    }
+    else
+    {
+      // Unknown argument
+      std::cerr << "Unknown argument: " << argument << std::endl;
+    }
   }
 }
 
